322a75a39270987042d0e38db160bec69dec70c3
Loreli <dejesus.loreli@gmail.com>
2023-05-04 21:42:27 +1000

    stopwatch seconds view

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,62 +1,62 @@
 import React from 'react';
+import './stopwatch.css'
 
 
 class Stopwatch extends React.Component {
-
+    
     constructor(props) {
     super(props);
 
     this.state = {
       timeCount: 0,
-      isRunning: false
+      isRunning: false,
+      intervalID: null
     };
+  }
 
-    // eslint-disable-next-line no-undef
+    
     watchStart = () => {
       this.setState({
       isRunning: true,
-      timeCount: this.timeCount + 1
-      })
+      timeCount: this.state.timeCount + 1,
+      intervalID: setInterval(() => {
+        this.setState({timeCount: this.state.timeCount + 1})}, 1000)
+      })}
+      
 
-      // eslint-disable-next-line no-undef
-      intervalID = setInterval(() => {
-      this.setState(this.state.timeCount, 1000)
-     })
-    }
-
-    // eslint-disable-next-line no-undef
     watchPause = () => {
-      // eslint-disable-next-line no-undef
-      clearInterval(this.intervalID);
       this.setState({
         timeCount: this.state.timeCount,
-        isRunning: false
-       })
-      }
-    }
-
-    watchReset = () => {
-      // eslint-disable-next-line no-undef
-      clearInterval(this.intervalID);
-      this.setState({
-        timeCount: 0,
-        isRunning: false
+        isRunning: false,
+        intervalID: clearInterval(this.state.intervalID)
        })
       }
     
-      render() {
-        const {timeCount, isRunning} = this.state;
-
-        return(
-          <div className='stopwatch'>
-            <div className='count'>Stopwatch:{timeCount} milliseconds</div>
-            {isRunning? 
-            (<button onClick={this.watchPause}>Pause</button>) :
-            (<button onClick={this.watchStart}>Start</button>)}
-            <button onClick={this.watchReset}>Reset</button>
-          </div>
-        )
+    watchReset = () => {
+      this.setState({
+        timeCount: 0,
+        isRunning: false,
+        intervalID: clearInterval(this.state.intervalID)
+       })
       }
-    }
+    
+     
+      render(){
+        const {timeCount, isRunning} = this.state;
+        
+    return (
+      <div className='stopwatch'>
+        <div className='count'>{timeCount} seconds</div>
+        <div className='button'>
+        {isRunning ? 
+          (<button className='dynamicButton' onClick={this.watchPause}>Pause</button>) :
+          (<button className='dynamicButton' onClick={this.watchStart}>Start</button>)}
+        <button className='resetButton' onClick={this.watchReset}>Reset</button>
+        </div>
+      </div>
+    );
+  }
+}
+    
 
 export default Stopwatch;
